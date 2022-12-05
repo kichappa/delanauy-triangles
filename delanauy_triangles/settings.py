f2e555affd501b32d79335a10d571b085db23966
kichappa <kichappa@gmail.com>
2022-12-05 17:10:11 +0000

    custom domain

--- delanauy_triangles/settings.py
+++ delanauy_triangles/settings.py
@@ -28,11 +28,13 @@ SECRET_KEY = os.environ.get('SECRET_KEY', default='your secret key')
 # DEBUG = True
 DEBUG = 'RENDER' not in os.environ
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["api2.ramdon.team"]
 
 RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
 if RENDER_EXTERNAL_HOSTNAME:
     ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
+
+print(ALLOWED_HOSTS)
 
 # Application definition
 
